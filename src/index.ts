b3fbaf421a143157fbca55a384cf023ca5c149ae
Dmitriy Shved <dshved@hotmail.com>
2018-08-22 22:10:34 +0300

    Move to ts

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as TelegramBot from "node-telegram-bot-api";
+
+import { whenBreakfast } from "./breakfast";
+
+import { token } from "../config";
+
+const bot = new TelegramBot(token, {
+  polling: true,
+});
+
+bot.onText(/\/start/, msg => {
+  if (msg.text !== "/start") return;
+  const chatId = msg.chat.id;
+  const message = "Чтобы узнать когда завтрак, напиши команду /breakfast";
+  bot.sendMessage(chatId, message);
+});
+
+bot.onText(
+  /\/breakfast|\/breakfast@SPBFrontendBreakfastBot/,
+  (msg, match: any) => {
+    const { chat } = msg;
+    if (match.length) {
+      const message = whenBreakfast();
+      if (message) {
+        bot.sendMessage(chat.id, message);
+      }
+    }
+  },
+);
