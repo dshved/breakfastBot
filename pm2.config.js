f87a33cc04627c40f490c37b2d82152f060e9441
Dmitry Shved <dshved@hotmail.com>
2018-09-06 11:36:56 +0300

    Update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "BreakfastBot",
-      script: "ts-node src/index.ts",
+      script: "./node_modules/ts-node/dist/bin.js",
+      args: "./src/index.ts",
       watch: true,
       ignore_watch: ["node_modules"],
     },
