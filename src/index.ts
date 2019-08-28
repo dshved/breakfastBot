3ee86a46ca2842d3640ca1a97f33095d599a88c6
Dmitriy Shved <dshved@hotmail.com>
2019-08-28 23:49:35 +0300

    Fix confglicts

--- src/index.ts
+++ src/index.ts
@@ -15,18 +15,7 @@ bot.onText(/\/start/, msg => {
   bot.sendMessage(chatId, message);
 });
 
-<<<<<<< HEAD
-bot.onText(
-  /\/breakfast|\/breakfast@SPBFrontendBreakfastBot/,
-  (msg, match: any) => {
-    const { chat, message_id } = msg;
-    if (match.length) {
-      const message = whenBreakfast();
-      if (message) {
-        bot.sendMessage(chat.id, message);
-        bot.deleteMessage(chat.id, message_id.toString());
-      }
-=======
+
 bot.onText(/\/breakfast|\/breakfast@SPBFrontendBreakfastBot/, (msg, match: any) => {
   const { chat, from, message_id } = msg;
   if (match.length) {
@@ -34,7 +23,6 @@ bot.onText(/\/breakfast|\/breakfast@SPBFrontendBreakfastBot/, (msg, match: any) 
     if (message) {
       bot.sendMessage(chat.id, message);
       bot.deleteMessage(chat.id, message_id.toString());
->>>>>>> 27099fed7f13801e85a32c4fb5bc5bc27a20cfa4
     }
   }
 });
