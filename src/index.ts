00259fb7bfb256f83d79ec213dc6c029266e55b0
Danakt Saushkin <danakt@protonmail.com>
2019-11-06 15:37:45 +0300

    Add notifying about frequent requests

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import { whenBreakfast } from "./breakfast";
 
 import { token } from "../config";
 
+const lastMessageData: { timestamp?: number; isNotifySent: boolean } = { timestamp: undefined, isNotifySent: false };
+const TEN_SECONDS = 10_000;
+
 const bot = new TelegramBot(token, {
-  polling: true,
+  polling: true
 });
 
 bot.onText(/\/start/, msg => {
@@ -15,14 +18,25 @@ bot.onText(/\/start/, msg => {
   bot.sendMessage(chatId, message);
 });
 
-
 bot.onText(/\/breakfast|\/breakfast@SPBFrontendBreakfastBot/, (msg, match: any) => {
-  const { chat, from, message_id } = msg;
+  const { chat, from } = msg;
+
   if (match.length) {
-    const message = whenBreakfast(from);
-    if (message) {
-      bot.sendMessage(chat.id, message);
-      bot.deleteMessage(chat.id, message_id.toString());
+    if (typeof lastMessageData.timestamp === "number" && Date.now() - TEN_SECONDS < lastMessageData.timestamp) {
+      if (!lastMessageData.isNotifySent) {
+        bot.sendMessage(chat.id, "Оставшееся время до завтрака можно запрашивать только раз в 10 секунд");
+
+        lastMessageData.isNotifySent = true;
+      }
+    } else {
+      const message = whenBreakfast(from);
+
+      if (message) {
+        bot.sendMessage(chat.id, message);
+
+        lastMessageData.timestamp = Date.now();
+        lastMessageData.isNotifySent = false;
+      }
     }
   }
 });
